3bf03e9a6919cda8ff6399ae31d9403db987c519
baquedano <wolhaiksong.baquedano@gmail.com>
2016-09-02 21:44:48 -0400

    arreglos

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -2,6 +2,50 @@ var SHA3 = require("crypto-js/sha3");
 var boom = require('boom');
 var room = require('../schemas/room');
 
+//set timer to restart the asigned rooms at 5 AM
+
+var date = new Date();
+var current_hour = date.getHours();
+var current_minutes = date.getMinutes();
+var current_seconds = date.getSeconds();
+
+var ms_in_day = 24*60*60*1000
+var ms_in_hour_to_reset = 19*60*60*1000 + 57*60*1000
+current_hour = current_hour* 60 * 60 *1000
+current_minutes = current_minutes * 60 * 1000
+current_seconds = current_seconds * 1000
+var current_time = current_hour+current_minutes+current_seconds
+var ms_till_reset 
+console.log(current_time)
+console.log(ms_in_hour_to_reset)
+if(current_time <= ms_in_hour_to_reset){
+  ms_till_reset = ms_in_hour_to_reset - current_time
+}else{
+  ms_till_reset = ms_in_day - current_time +  ms_in_hour_to_reset
+}
+console.log(ms_till_reset)
+setInterval(function() {
+     ms_till_reset = ms_in_day;
+     console.log("entreee")
+     var habitacion = room.find({},function(err,data){
+        for (var i = 0; i < data.length; i++) {
+           var habitacion = room.findOne({room_id:data[i].room_id},function(err,answer){
+            answer.status = 0,
+            answer.idUser = [],
+            answer.priority= answer.priority,
+            answer.observation=answer.observation,
+            answer.time_reserved =answer.time_reserved 
+            answer.save();
+          });
+
+        }
+    });
+
+
+}, ms_till_reset);
+
+//-----
+
 exports.createRoom = {
   /*auth: {
     mode:'required',
