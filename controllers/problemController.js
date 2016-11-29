bb212667a4a2fc1e9a7307c67f92db98b7f0275e
Dario Cardona <dario.cardona@unitec.edu>
2016-11-29 09:47:52 -0600

    controller de problema crud

--- controllers/problemController.js
+++ controllers/problemController.js
@@ -30,3 +30,32 @@ exports.createProblem = {
 		});
   	}
 }
+
+
+exports.deleteProblem={
+  //auth: {
+  //  mode:'required',
+  //  strategy:'session'
+  //},
+  handler: function(request, reply){
+    problema.findOneAndRemove({ problem_id:request.payload.problem_id }, function(err) {
+      if (err) {
+        throw err;
+      }
+      return reply('Eliminado exitosamente');
+    });
+  }
+};
+
+
+exports.modifProblem = {
+  handler: function(request, reply) {
+
+    var problema = problem.findOne({problem_id:request.payload.problem_id},function(err,answer){
+      answer.problem_description= request.payload.problem_description
+      answer.problem_type= request.payload.problem_type
+      answer.save();
+      return reply(answer);
+    });
+  }
+};
